359927a01b5ac0042c55edd1c3d5133cdaf66b25
osumi68 <asukasumie@gmail.com>
2024-04-14 18:50:49 +0900

    修正No3

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Item, type: :model do
   describe '#create' do
     before do
       @user = FactoryBot.create(:user)
-      @item = FactoryBot.build(:item, user: @user)
+      @item = FactoryBot.build(:item)
   end
   describe '商品出品機能' do
       context '商品名登録できる場合' do
