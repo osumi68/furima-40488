611d62917c9a4f07600297b35d5e1ffc7b183b5d
osumi68 <asukasumie@gmail.com>
2024-04-17 18:16:42 +0900

    削除機能 修正No2 Before action追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
   before_action :set_item, except: [:index, :new, :create]
   def index
     @items = Item.all.order(created_at: :desc)
