3376bc24d18e1fceced6b95d93f77c318c3f22d0
osumi68 <asukasumie@gmail.com>
2024-04-24 18:18:51 +0900

    アソシエーション定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,6 +11,7 @@ class Item < ApplicationRecord
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   belongs_to :user
+  has_many :comments
   has_one_attached :image
   has_one :order
   
